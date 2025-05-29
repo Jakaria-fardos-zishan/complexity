6a451ac494e667bec7293359e13467bd874784ce
Jakaria-fardos-zishan <fardos.zishan01@gmail.com>
2025-05-29 14:26:28 +0600

    Add files via upload

---
+++ xy_projecion_of_lorenz_system.m
@@ -0,0 +1,33 @@
+clc;
+a=20;
+b=8/3;
+c=28;
+x0=1;
+y0=5;
+z0=9;
+time=[0,500];
+
+[t,XYZ]=ode45(@(t,X) lorentz(t,X,a,b,c), time, [x0;y0;z0] );
+
+x=XYZ(:,1);
+y=XYZ(:,2);
+z=XYZ(:,3);
+figure; 
+plot (x,y, 'b');
+hold on;
+plot([min(x) max(x)], [min(x) max(x)],'LineWidth', 1.5 );
+hold off;
+
+function dXdt= lorentz (~,X,a,b,c)
+x=X(1);
+y=X(2);
+z=X(3);
+
+
+dx= a*(y - x);
+dy = c*x - x*z - y;
+dz = x*y - b*z;
+
+dXdt=[dx;dy;dz];
+end
+
